352cc8ff4846b66b4c4d8c489844f46ac4b751c9
Lukáš Kumpan <lukylinek@gmail.com>
2025-11-12 17:46:51 +0100

    Add input validation and scene function

--- main.cpp
+++ main.cpp
@@ -1,48 +1,63 @@
 #include <iostream>
-#include <string>
-#include <vector>
+#include <limits>
 using namespace std;
 
-// Struktura pro jedno místo
-struct Lokace {
-    string jmeno;
-    string popis;
+// funkce co čte volby
+int ctiVolbu(int minimum, int maximum) {
+    int cislo;
 
-// Ukazatele na jiné lokace
-Lokace* nahoru;
-Lokace* dolu;
-Lokace* doleva;
-Lokace* doprava;
+    while (true) {
+        cout << "> ";
+        cin >> cislo;
 
-    bool maPredmet;
-    string predmet;
-};
+        // pokud nekdo napíše písmeno nebo blbost napr a nebo 10
+        if (!cin) {
+            cout << "Zadej cislo!" << endl;
+            cin.clear(); 
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
+            continue;
+        }
 
-// Inventář hráče
-struct Hrac {
-    bool bomba = false;
-    bool stipacky = false;
-    bool munice = false;
-    bool bren = false;
-};
+        // kontroluje rozsahu
+        if (cislo >= minimum and cislo <= maximum) {
+            return cislo;
+        }
 
-// Struktura zatím jen s názvem
-struct Area {
-    string name;
-};
+        cout << "Zadej cislo " << minimum << "-" << maximum << endl;
+    }
+}
 
-// Seznam oblastí
-vector<Area> mapa = {
-    {"Okraj lesa (únik)"},
-    {"Rádiová věž"},
-    {"Minové pole"},
-    {"Zřícená silnice"},
-    {"Protitankové dělo"},
-    {"Lesní stezka"},
-    {"Vojenské skladiště"},
-    {"Zničený most"},
-    {"Spawn"},
-    {"Tábor nepřátel"},
-    {"Okraj lesa (fake)"}
-};
+void scena1() {
+    cout << endl;
+    cout << "SCENA 1 - Jednani s agentem" << endl;
+    cout << "Vaclav: \"Dusan, mam tu nabidku z FC Drakonia.\"\n";
+    cout << "Trileta smlouva, 85 tisic mesicne a premie za goly." << endl;
+    cout << "Co myslis?" << endl;
+    cout << "1) Podepis to, chci hrat!" << endl;
+    cout << "2) Zkus vyjednat vic." << endl;
+    cout << "3) Neni to malo?" << endl;
 
+    int volba = ctiVolbu(1, 3);
+
+    if (volba == 1) {
+        cout << "Vaclav: \"Beru to. Zitra tiskovka.\"";
+    }
+    else if (volba == 2) {
+        cout << "Vaclav: \"Ok, zkusim zatlacit. Zitra to potvrdime.\"";
+    }
+    else {
+        cout << "Vaclav: \"Hele, Drakonia je dobra sance. Promysli to.\"";
+    }
+
+    cout << endl << endl << "[Konec sceny 1]" << endl;
+}
+
+int main() {
+    cout << "KONTRAKT - textova hra" << endl;
+    cout << "(Volby zadavej cislem 1/2/3)" << endl;
+
+    scena1();  
+
+    cout << endl << "Diky za hrani " << endl;
+    return 0;
+}
